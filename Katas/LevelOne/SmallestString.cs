f6e3f8ebce946779ea9e404fcee44d4dcfff9349
Krauser123 <66463181+Krauser123@users.noreply.github.com>
2021-03-23 16:13:00 +0100

    Update SmallestString.cs

--- Katas/LevelOne/SmallestString.cs
+++ Katas/LevelOne/SmallestString.cs
@@ -9,15 +9,29 @@ namespace Katas.LevelOne
     public class SmallestString
     {
 
-        //Given a int 'weight' return the smallest string, all chars have a weight equal to its position on dictionary.
+        //Given a int 'weight' return the smallest string, all chars have a weight equal to its position on ABC.
 
-        private string GetSmallestString(int weight)
+        public string GetSmallestString(int weight)
         {
             string word = string.Empty;
 
-
+            for (int i = 26; i > 0; i--)
+            {
+                while (weight > i)
+                {
+                    char letter = GetLetterByWeight(i);
+                    word += letter;
+                    weight -= i;
+                }
+            }
 
             return word;
+        }
+
+        private char GetLetterByWeight(int weight)
+        {
+            var index = (char)(weight + 64);
+            return index;
         }
     }
 }
