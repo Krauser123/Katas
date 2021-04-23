eba335e29e9b1776ef67d5f29a985349365978b2
Krauser123 <66463181+Krauser123@users.noreply.github.com>
2021-04-23 10:44:45 +0200

    Update ReverseWordsInAStringTest.cs

--- Katas.TestProject/LevelOne/ReverseWordsInAStringTest.cs
+++ Katas.TestProject/LevelOne/ReverseWordsInAStringTest.cs
@@ -10,8 +10,40 @@ namespace Katas.TestProject.LevelOne
         public void IsReverseRight()
         {
             ReverseWordsInAString reverseWordsInAString = new ReverseWordsInAString();
-            
-            Assert.IsTrue(false);
+            string reversedPhrase = reverseWordsInAString.ReverseWords("Interviews are awesome!");
+            Assert.AreEqual("awesome! are Interviews", reversedPhrase);
+        }
+
+        [TestMethod]
+        public void ReversedPhraseIsNotNull()
+        {
+            ReverseWordsInAString reverseWordsInAString = new ReverseWordsInAString();
+            string reversedPhrase = reverseWordsInAString.ReverseWords("Interviews are awesome!");
+            Assert.IsNotNull(reversedPhrase);
+        }
+
+        [TestMethod]
+        public void RemoveWhiteSpaces()
+        {
+            ReverseWordsInAString reverseWordsInAString = new ReverseWordsInAString();
+            string reversedPhrase = reverseWordsInAString.ReverseWords("  Hi  ");
+            Assert.AreEqual("Hi", reversedPhrase);
+        }
+
+        [TestMethod]
+        public void MultipleWhiteSpaces()
+        {
+            ReverseWordsInAString reverseWordsInAString = new ReverseWordsInAString();
+            string reversedPhrase = reverseWordsInAString.ReverseWords("  Hi      Test    Method   ");
+            Assert.AreEqual("Method Test Hi", reversedPhrase);
+        }
+
+        [TestMethod]
+        public void PhraseIsNull()
+        {
+            ReverseWordsInAString reverseWordsInAString = new ReverseWordsInAString();
+            string reversedPhrase = reverseWordsInAString.ReverseWords(null);
+            Assert.IsNull(reversedPhrase);
         }
     }
 }
